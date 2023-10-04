89de2bb2ddbbc750b28adc88fada283fc3138c13
Richard-Anthony-Santoso <122846268+Richard-Anthony-Santoso@users.noreply.github.com>
2023-10-04 20:14:53 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('btn')
+
+function leftClick() {
+	btn.style.left = '0'
+	onclick = location.href = '#home'
+}
+
+function rightClick() {
+	btn.style.left = '150px'
+	onclick = location.href = '#about'
+}
---
+++ header.js
@@ -0,0 +1,10 @@
+const header = document.querySelector(".page-header");
+const toggleClass = "is-sticky";
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
